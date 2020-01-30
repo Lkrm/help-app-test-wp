f732f0179aba52a5304a45ad4f7f3e0103c1fdcc
biktor12487 <biktor12487@gmail.com>
2020-01-30 15:56:56 +0200

    Service module

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Services from './Services';
-
-
-ReactDOM.render(<Services/>, document.getElementById('root'));
-
-// serviceWorker.register();
