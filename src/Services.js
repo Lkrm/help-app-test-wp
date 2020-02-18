6af821b11a2145f7f610b847e8029432107412e0
biktor12487 <biktor12487@gmail.com>
2020-02-18 14:12:07 +0200

    fix

--- src/Services.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Services = () => (<h2>This is component of child module `module-two`, - <i>Some changes at services</i></h2>);
-
-export default Services;
---
+++ src/components/HappyComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HappyComponent = () => (<h1>Some very happy page =)</h1>);
+
+export default HappyComponent;
